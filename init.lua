fe0fb34af540dc564640ac822d1ad338c401ddc2
Lucas Vasconcelos <lucas.av7@yahoo.com>
2025-01-15 10:29:19 -0300

    Use pyright instead of pylsp

--- init.lua
+++ init.lua
@@ -610,9 +610,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pylsp = {
-          configurationSources = {},
-        },
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
