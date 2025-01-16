af86c53c2a97752fea3e9888df90edd99f948b53
Lucas Vasconcelos <lucas.av7@yahoo.com>
2025-01-16 13:02:07 -0300

    Use prettierd instead of prettier

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -182,7 +182,7 @@ return {
         pyright = {},
         black = {},
         isort = {},
-        prettier = {},
+        prettierd = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
