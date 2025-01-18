7f0fe6cc3ce5dbdda9e9b7b10fadd2ebe2fc92cd
Lucas Vasconcelos <lucas.av7@yahoo.com>
2025-01-18 12:53:16 -0300

    Install noice

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      lsp = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+    },
+    keys = {
+      { '<leader>sl', '<cmd>Noice telescope<CR>', mode = 'n', silent = true, desc = '[S]earch [L]ogs' },
+    },
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+      'hrsh7th/nvim-cmp',
+      'nvim-telescope/telescope.nvim',
+    },
+  },
+}
