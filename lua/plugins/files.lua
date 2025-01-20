8febc9fd873beb50eeb8a2ca9275b473a759fd70
Lucas Vasconcelos <lucas.av7@yahoo.com>
2025-01-20 11:43:05 -0300

    Open mini.files in current buffer

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -7,7 +7,7 @@ return {
       {
         '<leader>f',
         function()
-          require('mini.files').open()
+          require('mini.files').open(vim.api.nvim_buf_get_name(0))
         end,
         mode = 'n',
         silent = true,
