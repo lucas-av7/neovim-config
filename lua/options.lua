2d9125a82993d7132c9b755eea8fd544dd0ff3b2
Lucas Vasconcelos <lucas.av7@yahoo.com>
2025-02-19 15:54:03 -0300

    Set autored true

--- lua/options.lua
+++ lua/options.lua
@@ -4,6 +4,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Auto refresh buffer
+vim.opt.autoread = true
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
