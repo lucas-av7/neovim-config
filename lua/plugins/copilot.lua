23eb1fa815bd1c5881eb81627a7bc8c9c0ca8ace
Lucas Vasconcelos <lucas.av7@yahoo.com>
2025-12-03 16:54:58 -0300

    Updates on copilot plugin

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,28 +1,15 @@
 return {
-  {
-    'github/copilot.vim',
-    enabled = false,
-    cmd = 'Copilot',
-    event = 'InsertEnter',
-    tag = 'v1.38.0', -- For Node.js 16 compatibility
+  'zbirenbaum/copilot.lua',
+  enabled = true,
+  requires = {
+    'copilotlsp-nvim/copilot-lsp', -- (optional) for NES functionality
   },
-  {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    enabled = false,
-    cmd = 'CopilotChat',
-    dependencies = {
-      { 'github/copilot.vim' },
-      { 'nvim-lua/plenary.nvim' },
-    },
-    build = 'make tiktoken',
-    opts = {},
-    keys = {
-      {
-        '\\',
-        '<cmd>CopilotChatToggle<cr>',
-        mode = { 'n', 'v' },
-        desc = 'Copilot Chat',
-      },
-    },
-  },
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  config = function()
+    require('copilot').setup {
+      copilot_node_command = os.getenv 'HOME' .. '/.local/share/mise/installs/node/latest/bin/node',
+      suggestion = { auto_trigger = true },
+    }
+  end,
 }
